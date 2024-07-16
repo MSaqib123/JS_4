956c0a737539ed11ae6cb005c3ea99a2d271dfdd
M Boota <48630668+MSaqib123@users.noreply.github.com>
2024-07-16 18:37:06 +0500

    which Data Structure to use

---
+++ 11. Object vs Map and Array vs Sets.js
@@ -0,0 +1,49 @@
+//=============================== Object vs Maps  and  Arrays vs Sets ====================================
+//which data structure to use
+
+
+//Source of Data
+//1. from the program it self .  writen statically in function or program
+//2. from DOM ,  data is being coming from input or anything
+//3. from API ,  from any other  Application Programing interface
+
+//it can be
+//1. Collection of Data
+//2. Data Strucatur
+
+//Design
+//1. if  we need  simple list of Values then   ====> Array or Sets
+//2. if we need key value pair   ============> Object or Map
+
+
+//============== Array vs  Sets ====================
+
+//------- Array ----------
+//1. use when you need ordered list of value (might contain duplicates)
+//2. use when you need to manipulate data
+
+//------- Sets ----------
+//1. use when you need to work with unique vlaues
+//2. Use when hight-performance is realy important
+//3. use to remove duplicates from arrays
+
+
+//============== obj vs Map ====================
+
+//------- Array ----------
+//1. More 'traditional' key/value store (abused objects)
+//2. Easier to write and accesss value with . and []
+
+//=> Use When you need to  include function (method)
+//=> Use when  working with JSON (can convert to map)
+
+//------- Maps ----------
+//1. Better performance
+//2. key can have any data type
+//3. Easy to iterate
+//4. Easy to compute size
+
+
+//=> use when you simple need to map key to values
+//=> Use when you need keys that are not strings
+
