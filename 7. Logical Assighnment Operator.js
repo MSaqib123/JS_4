853e7c076a08afcb60a08c35049987be4e353056
M Boota <48630668+MSaqib123@users.noreply.github.com>
2024-07-16 15:29:16 +0500

    logical operators

--- 7. Logical Assighnment Operator.js
+++ 7. Logical Assighnment Operator.js
@@ -28,9 +28,8 @@ console.log(emp2.age)
 //______________ And Assignment _________________
 // emp1.owner = emp1.owner && '<ANNOYMOUSE' //undefined because its not in    empl1
 // emp2.owner = emp2.owner && '<ANNOYMOUSE'
-
-emp1.owner &&= '<ANNOYMOUSE'
-emp2.owner &&= '<ANNOYMOUSE'
+emp1.owner &&= '<ANNOYMOUSE>'
+emp2.owner &&= '<ANNOYMOUSE>'
 
 console.log(emp1)
 console.log(emp2)
